72ec267319615e220ceab6ee9963724bb455cc34
aartigholam <55346949+aartigholam@users.noreply.github.com>
2021-04-24 15:18:19 +0530

    Update SearchPage.java

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -5,5 +5,9 @@ public class SearchPage {
 	public void Searchpage() {
 		System.out.println("Search page");
 	}
+	
+	public void AdvancedSearchpage() {
+		System.out.println("Advanced Search page");
+	}
 
 }
